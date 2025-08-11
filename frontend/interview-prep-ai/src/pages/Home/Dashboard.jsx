376f89607e2247cd155c2e0e4c3b12533f94b8a1
Manasa Reddy <kandadimanasareddy12@gmail.com>
2025-08-11 22:20:39 +0530

    Update Dashboard.jsx

--- frontend/interview-prep-ai/src/pages/Home/Dashboard.jsx
+++ frontend/interview-prep-ai/src/pages/Home/Dashboard.jsx
@@ -3,7 +3,7 @@ import { LuPlus } from 'react-icons/lu'
 import { CARD_BG } from "../../utils/data";
 import toast from 'react-hot-toast';
 import DashboardLayout from '../../components/Layouts/DashboardLayout';
-import { data, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import axiosInstance from '../../utils/axiosInstance';
 import { API_PATHS } from '../../utils/apiPaths';
 import SummaryCard from '../../components/Cards/SummaryCard';
@@ -11,28 +11,34 @@ import moment from "moment";
 import Modal from '../../components/Modal';
 import CreateSessionForm from './CreateSessionForm';
 import DeleteAlertContent from '../../components/DeleteAlertContent';
+import DashboardSkeleton from "../../components/Loader/DashboardSkeleton";
 
 const Dashboard = () => {
   const navigate = useNavigate();
 
   const [openCreateModal, setOpenCreateModal] = useState(false);
   const [sessions, setSessions] = useState([]);
+  const [loading, setLoading] = useState(true); // NEW
 
-  const [openDeleteAlert, setOpenDeleteAlert ] = useState({
+  const [openDeleteAlert, setOpenDeleteAlert] = useState({
     open: false,
     data: null,
   });
+
   const fetchAllSessions = async () => {
-    try{
+    try {
+      setLoading(true); // start loading before API call
       const response = await axiosInstance.get(API_PATHS.SESSION.GET_ALL);
       setSessions(response.data);
-    }catch(error){
-      console.error("Error fetching session data:",error);
+    } catch (error) {
+      console.error("Error fetching session data:", error);
+    } finally {
+      setLoading(false); // stop loading after API call
     }
   };
 
   const deleteSession = async (sessionData) => {
-    try{
+    try {
       await axiosInstance.delete(API_PATHS.SESSION.DELETE(sessionData?._id));
 
       toast.success("Session Deleted Successfully");
@@ -41,8 +47,8 @@ const Dashboard = () => {
         data: null,
       });
       fetchAllSessions();
-    }catch(error){
-      console.error("Error deleting session data:",error);
+    } catch (error) {
+      console.error("Error deleting session data:", error);
     }
   };
 
@@ -50,11 +56,18 @@ const Dashboard = () => {
     fetchAllSessions();
   }, []);
 
-  useEffect
+  if (loading) {
+    return (
+      <DashboardLayout>
+        <DashboardSkeleton />
+      </DashboardLayout>
+    );
+  }
+
   return (
     <DashboardLayout>
       <div className='container mx-auto pt-4 pb-4'>
-        <div className='grid grid-cols-1 nd:grid-cols-3 gap-4 nd:gap-7 pt-1 pb-6 px-4 nd:px-0'>
+        <div className='grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-7 pt-1 pb-6 px-4 md:px-0'>
           {sessions?.map((data, index) => (
             <SummaryCard
               key={data?._id}
@@ -74,39 +87,43 @@ const Dashboard = () => {
             />
           ))}
         </div>
-        <button className='h-12 nd:h-12 flex items-center justify-center gap-3 bg-linear-to-r from-[#FF9324] to-[#e99a4b] text-sm font-semibold px-7 py-2.5 rounded-full hover:bg-black hover:text-white transition-colors cursor-pointer hover:shadow-2xl hover:shadow-orange-300 fixed bottom-10 nd:bottom-20 right-10 nd:right-20'
-        onClick={() => setOpenCreateModal(true)}>
-          <LuPlus className='text-2xl text-white'/>
+        <button
+          className='h-12 flex items-center justify-center gap-3 bg-gradient-to-r from-[#FF9324] to-[#e99a4b] text-sm font-semibold px-7 py-2.5 rounded-full hover:bg-black hover:text-white transition-colors cursor-pointer hover:shadow-2xl hover:shadow-orange-300 fixed bottom-10 md:bottom-20 right-10 md:right-20'
+          onClick={() => setOpenCreateModal(true)}
+        >
+          <LuPlus className='text-2xl text-white' />
           Add New
         </button>
       </div>
 
       <Modal
-      isOpen={openCreateModal}
-      onClose={() => {
-        setOpenCreateModal(false);
-      }}
-      hideHeader>
+        isOpen={openCreateModal}
+        onClose={() => {
+          setOpenCreateModal(false);
+        }}
+        hideHeader
+      >
         <div>
-          <CreateSessionForm/>
+          <CreateSessionForm />
         </div>
       </Modal>
 
       <Modal
-      isOpen={openDeleteAlert?.open}
-      onClose={() => {
-        setOpenCreateModal({open: false, data:null});
-      }}
-      title="Delete Alert">
-        <div className='w-[30vw]'>
+        isOpen={openDeleteAlert?.open}
+        onClose={() => {
+          setOpenDeleteAlert({ open: false, data: null });
+        }}
+        title="Delete Alert"
+      >
+        <div className='w-full md:w-[30vw]'>
           <DeleteAlertContent
-          content="Are you sure you want to delete this session detail?"
-          onDelete={()=> deleteSession(openDeleteAlert.data)}
+            content="Are you sure you want to delete this session detail?"
+            onDelete={() => deleteSession(openDeleteAlert.data)}
           />
         </div>
       </Modal>
     </DashboardLayout>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
