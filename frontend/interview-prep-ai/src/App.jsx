cdb7f569c1b5f6ec548e1021b413282f75aaa9b7
Manasa Reddy <kandadimanasareddy12@gmail.com>
2025-08-11 22:27:25 +0530

    Update App.jsx

--- frontend/interview-prep-ai/src/App.jsx
+++ frontend/interview-prep-ai/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import {Toaster} from 'react-hot-toast'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
 
 import LandingPage from './pages/LandingPage';
 import Login from './pages/Auth/Login';
@@ -8,34 +8,50 @@ import SignUp from './pages/Auth/SignUp';
 import Dashboard from './pages/Home/Dashboard';
 import InterviewPrep from './pages/InterviewPrep/InterviewPrep';
 import UserProvider from './context/UserContext';
+import ProtectedRoute from "./components/ProtectedRoute";
 
 const App = () => {
   return (
     <UserProvider>
-    <div>
-      <Router>
-        <Routes>
-          {/* Default Route */}
-          <Route path='/' element={<LandingPage/>}/>
+      <div>
+        <Router>
+          <Routes>
+            {/* Default Route */}
+            <Route path='/' element={<LandingPage />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/signup' element={<SignUp />} />
 
-            <Route path='/login' element={<Login/>}/>
-            <Route path='/signup' element={<SignUp/>}/>
-            <Route path='/dashboard' element={<Dashboard/>}/>
-            <Route path='/interview-prep/:sessionId' element={<InterviewPrep/>}/>
-        </Routes>
-      </Router>
+            {/* Protected Routes */}
+            <Route
+              path='/dashboard'
+              element={
+                <ProtectedRoute>
+                  <Dashboard />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path='/interview-prep/:sessionId'
+              element={
+                <ProtectedRoute>
+                  <InterviewPrep />
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+        </Router>
 
-      <Toaster
-        toastOptions={{
-          className: "",
-          style:{
-            fontSize: "13px",
-          },
-        }}
+        <Toaster
+          toastOptions={{
+            className: "",
+            style: {
+              fontSize: "13px",
+            },
+          }}
         />
-    </div>
+      </div>
     </UserProvider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
